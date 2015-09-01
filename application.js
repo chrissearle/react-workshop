49e32f0f8225400ef2c22762048d5ebc5a10c57b
Jonas Amundsen <jonasba@gmail.com>
2015-09-01 11:00:36 +0200

    Add a basic example for showing how to use Browserify

---
+++ application.js
@@ -0,0 +1,6 @@
+var multiply = require("./multiply");
+
+var square = require("./square");
+
+console.log("The mulitple of 3 and 3 is", multiply(3, 3));
+console.log("The square of 3 and 3 is", square(3, 3));
---
+++ square.js
@@ -0,0 +1,5 @@
+function square (a, b) {
+  return Math.pow(a, b);
+}
+
+module.exports = square;
